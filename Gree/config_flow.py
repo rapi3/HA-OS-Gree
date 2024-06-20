a3faa9ef6df2b3a769a7d4950fad8f3f5bdfee6b
rapi3 <18531150+rapi3@users.noreply.github.com>
2024-06-20 21:11:20 +0300

    Add files via upload

---
+++ Gree/config_flow.py
@@ -0,0 +1,75 @@
+"""Config flow for Gree."""
+from ipaddress import IPv4Address
+import logging
+from typing import Any
+
+from greeclimate.discovery import Discovery
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.components.network import async_get_ipv4_broadcast_addresses
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.exceptions import HomeAssistantError
+
+from .const import DISCOVERY_TIMEOUT, DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Optional("ip"): str,
+    }
+)
+
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]):
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+
+    gree_discovery = Discovery(DISCOVERY_TIMEOUT)
+    if "ip" in data and data["ip"]:
+        bcast_addr = [IPv4Address(ip.strip()) for ip in str(data["ip"]).split(",")]
+    else:
+        bcast_addr = list(await async_get_ipv4_broadcast_addresses(hass))
+    devices = await gree_discovery.scan(
+        wait_for=DISCOVERY_TIMEOUT, bcast_ifaces=bcast_addr
+    )
+    if len(devices) < 1:
+        raise CannotConnect
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Gree."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            try:
+                await validate_input(self.hass, user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                if "ip" in user_input and user_input["ip"]:
+                    title = user_input["ip"]
+                else:
+                    title = "HomeAssistant"
+                return self.async_create_entry(title=title, data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
